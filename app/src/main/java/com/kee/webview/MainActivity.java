ec4c76bbd74d1a19d2c258119639824b833aca8d
Vladimir Leonov <43324348+Fut1le@users.noreply.github.com>
2020-02-02 19:12:32 +0300

    Add files via upload

---
+++ app/src/main/java/com/kee/webview/MainActivity.java
@@ -0,0 +1,44 @@
+package com.kee.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // set transparent status bar
+        // getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        // set view
+        webView = findViewById(R.id.my_webview);
+
+        // configure settings
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // set web view client
+        webView.setWebViewClient(new WebViewClient());
+
+        // set url
+        webView.loadUrl("https://github.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
